acc1d9a9db51f59da366853ceace7c88dd614fdd
ioanna <ioannavrachni@gmail.com>
2024-09-25 19:33:37 +0300

    My First Commit

---
+++ fitnessapp/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:fitnessapp/common/color_extension.dart';
+import 'package:fitnessapp/view/onBoarding/on_boarding_view.dart';
+import 'package:fitnessapp/view/onBoarding/started_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FitnessApp',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: TColor.primaryColor1,
+        fontFamily: "Poppins",
+        useMaterial3: true,
+      ),
+      home: const StartedView(),
+    );
+  }
+}
