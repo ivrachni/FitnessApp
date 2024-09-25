c993b5f10a7d71a868db67839346480cadb3fa7a
ioanna <ioannavrachni@gmail.com>
2024-09-25 23:43:15 +0300

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FitnessApp',
+      title: 'FitnesssApp',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: TColor.primaryColor1,
