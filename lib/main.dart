674a5294103af85d49d5701014ce33a2537f5ed0
ioanna <ioannavrachni@gmail.com>
2024-09-26 00:10:00 +0300

    created a new fitness app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FitnesssApp',
+      title: 'FitnessApp',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: TColor.primaryColor1,
